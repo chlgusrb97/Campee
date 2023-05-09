1f0af14150bf49e0d9b1f132491d4067e88e2211
chlgusrb97 <ikolp34@gmail.com>
2023-05-10 00:09:27 +0900

    Design 댓글 목록 UI

---
+++ src/components/units/products/detail/comment/list/comment.list.index.tsx
@@ -0,0 +1,54 @@
+import * as S from "./comment.list.styles";
+
+export default function ProductsDeTailCommentListUI() {
+  return (
+    <>
+      <S.Wrapper>
+        <li>
+          <S.Name>홍길동</S.Name>
+          <S.Contents>
+            <p>사이즈가 더 큰 건 없나요??</p>
+            <div>
+              <p>2023. 3. 30</p>
+              <S.Edit />
+              <S.Delete />
+            </div>
+          </S.Contents>
+        </li>
+        <li>
+          <S.Name>홍길동</S.Name>
+          <S.Contents>
+            <p>사이즈가 더 큰 건 없나요??</p>
+            <div>
+              <p>2023. 3. 30</p>
+              <S.Edit />
+              <S.Delete />
+            </div>
+          </S.Contents>
+        </li>
+        <li>
+          <S.Name>홍길동</S.Name>
+          <S.Contents>
+            <p>사이즈가 더 큰 건 없나요??</p>
+            <div>
+              <p>2023. 3. 30</p>
+              <S.Edit />
+              <S.Delete />
+            </div>
+          </S.Contents>
+        </li>
+        <li>
+          <S.Name>홍길동</S.Name>
+          <S.Contents>
+            <p>사이즈가 더 큰 건 없나요??</p>
+            <div>
+              <p>2023. 3. 30</p>
+              <S.Edit />
+              <S.Delete />
+            </div>
+          </S.Contents>
+        </li>
+      </S.Wrapper>
+    </>
+  );
+}
