361599dc35e10cdabcacc0599c91c7b487077383
chlgusrb97 <ikolp34@gmail.com>
2023-05-10 16:09:07 +0900

    Feat 댓글 답변 기능 try-catch 구문 예외처리

--- src/components/commons/customs/hooks/useCreateUsedItemAnswer.ts
+++ src/components/commons/customs/hooks/useCreateUsedItemAnswer.ts
@@ -2,6 +2,7 @@ import {UseFormSetValue} from "react-hook-form";
 import {ICreateUseditemQuestionAnswerInput} from "../../../../commons/types/generated/types";
 import {useMutationCreateUsedItemAnswer} from "../useMutations/useMutations";
 import {Dispatch, SetStateAction} from "react";
+import {Modal} from "antd";
 
 export const useCreateUsedItemAnswer = () => {
   const [createUseditemQuestionAnswer] = useMutationCreateUsedItemAnswer();
@@ -13,17 +14,23 @@ export const useCreateUsedItemAnswer = () => {
       setIsAnswer: Dispatch<SetStateAction<boolean>>
     ) =>
     async (data: ICreateUseditemQuestionAnswerInput): Promise<void> => {
-      const result = await createUseditemQuestionAnswer({
-        variables: {
-          useditemQuestionId,
-          createUseditemQuestionAnswerInput: {
-            contents: data.contents,
+      try {
+        const result = await createUseditemQuestionAnswer({
+          variables: {
+            useditemQuestionId,
+            createUseditemQuestionAnswerInput: {
+              contents: data.contents,
+            },
           },
-        },
-      });
-      setIsAnswer(false);
-      setValue("contents", "");
-      console.log(result, "답글 작성!");
+        });
+        setIsAnswer(false);
+        setValue("contents", "");
+        console.log(result, "답글 작성!");
+      } catch (error) {
+        Modal.error({
+          content: "댓글 답변 등록에 실패하였습니다. 다시 시도해주세요.",
+        });
+      }
     };
 
   return {createAnswerSubmit};
