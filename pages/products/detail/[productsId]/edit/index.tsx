dbbb6daa748fc9d90d4f1cff20e5875dd9b04464
chlgusrb97 <ikolp34@gmail.com>
2023-05-10 19:08:56 +0900

    Feat 권한분기

--- pages/products/detail/[productsId]/edit/index.tsx
+++ pages/products/detail/[productsId]/edit/index.tsx
@@ -1,8 +1,10 @@
+import {useAuth} from "../../../../../src/components/commons/customs/useAuth";
 import {useQueryUsedItem} from "../../../../../src/components/commons/customs/useQueries.ts/useQueries";
 import ProductsRegistrationUI from "../../../../../src/components/units/products/registration/products.registration.index";
 
 export default function ProductsEditPage() {
   const {usedItemData} = useQueryUsedItem();
+  useAuth();
 
   return <ProductsRegistrationUI isEdit={true} usedItemData={usedItemData} />;
 }
