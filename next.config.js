e103277a77ce73982d40e86c9202130ce6af7dc9
chlgusrb97 <ikolp34@gmail.com>
2023-07-07 05:34:28 +0900

    Chore 빌드 관련 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  trailingSlash: true,
+  generateBuildId: () => "Kyu-Campee-project",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
