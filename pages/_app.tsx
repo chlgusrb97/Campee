7d43535a29605cdfe1fdc5a055e81a58d102d86d
chlgusrb97 <ikolp34@gmail.com>
2023-07-08 19:32:08 +0900

    Design 파비콘 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,16 @@ import {RecoilRoot} from "recoil";
 import ApolloSetting from "../src/components/commons/apollo/apollo.index";
 import {globalStyles} from "../src/commons/styles/globalStyles";
 import Layout from "../src/components/commons/layout/layout.index";
+import Head from "next/head";
 
 export default function App({Component}: AppProps) {
   return (
     <RecoilRoot>
       <ApolloSetting>
         <>
-          <link rel="icon" href="/favicon.ico" sizes="20px" />
+          <Head>
+            <link rel="icon" href="/favicon.ico" sizes="20px" />
+          </Head>
           <Global styles={globalStyles} />
           <Layout>
             <Component />
