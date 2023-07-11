6aedbefec0c49d4844ec50dcc8b97491b17ee51a
chlgusrb97 <ikolp34@gmail.com>
2023-07-12 03:51:28 +0900

    Breaking Change DB API 주소 변경

--- src/commons/libraries/getAccessToken.ts
+++ src/commons/libraries/getAccessToken.ts
@@ -17,7 +17,7 @@ const RESTORE_ACCESS_TOKEN = gql`
 export const getAccessToken = async () => {
   try {
     const graphQLClient = new GraphQLClient(
-      "https://backend-practice.codebootcamp.co.kr/graphql",
+      "https://backend11.codebootcamp.co.kr/graphql07",
       {credentials: "include"}
     );
     const result = await graphQLClient.request<IRestoreAccessToken>(
